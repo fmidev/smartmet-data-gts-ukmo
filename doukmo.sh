bdb18befaa090fe9d1c980f0a195d5d07557d50e
Mikko Rauhala <mikko@rauhala.net>
2018-12-10 10:19:42 +0200

    Initial version

---
+++ doukmo.sh
@@ -0,0 +1,178 @@
+#!/bin/sh
+#
+# Finnish Meteorological Institute / Mikko Rauhala (2018)
+#
+# SmartMet Data Ingestion Module for UKMO Global Model
+#
+
+# Load Configuration 
+if [ -s /smartmet/cnf/data/ukmo.cnf ]; then
+    . /smartmet/cnf/data/ukmo.cnf
+fi
+
+if [ -s ukmo.cnf ]; then
+    . ./ukmo.cnf
+fi
+
+# Setup defaults for the configuration
+
+if [ -z "$AREA" ]; then
+    AREA=world
+fi
+
+if [ -z "$PROJECTION" ]; then
+    PROJECTION=""
+else 
+    PROJECTION="-P $PROJECTION"
+fi
+
+while getopts  "a:dp:" flag
+do
+  case "$flag" in
+        a) AREA=$OPTARG;;
+        d) DRYRUN=1;;
+        p) PROJECTION=$OPTARG;;
+  esac
+done
+
+STEP=6
+# Model Reference Time
+RT=`date -u +%s -d '-2 hours'`
+RT="$(( $RT / ($STEP * 3600) * ($STEP * 3600) ))"
+RT_HOUR=`date -u -d@$RT +%H`
+RT_DATE_MMDD=`date -u -d@$RT +%Y%m%d`
+RT_DATE_MMDDHH=`date -u -d@$RT +%m%d%H`
+RT_DATE_DDHH=`date -u -d@$RT +%d%H00`
+RT_DATE_HH=`date -u -d@$RT +%Y%m%d%H`
+RT_DATE_HHMM=`date -u -d@$RT +%Y%m%d%H%M`
+RT_ISO=`date -u -d@$RT +%Y-%m-%dT%H:%M:%SZ`
+
+if [ -d /smartmet ]; then
+    BASE=/smartmet
+else
+    BASE=$HOME/smartmet
+fi
+
+IN=$BASE/data/incoming/gts/ukmo
+OUT=$BASE/data/ukmo/$AREA
+CNF=$BASE/run/data/ukmo_gts/cnf
+EDITOR=$BASE/editor/in
+TMP=$BASE/tmp/data/ukmo_${AREA}_${RT_DATE_HHMM}
+LOGFILE=$BASE/logs/data/ukmo_${AREA}_${RT_HOUR}.log
+
+OUTNAME=${RT_DATE_HHMM}_ukmo_$AREA
+
+OUTFILE_SFC=$OUT/surface/querydata/${OUTNAME}_surface.sqd
+OUTFILE_PL=$OUT/pressure/querydata/${OUTNAME}_pressure.sqd
+OUTFILE_ML=$OUT/hybrid/querydata/${OUTNAME}_hybrid.sqd
+
+# Use log file if not run interactively
+if [ $TERM = "dumb" ]; then
+    exec &> $LOGFILE
+fi
+
+echo "Model Reference Time: $RT_ISO"
+echo "Projection: $PROJECTION"
+echo "Temporary directory: $TMP"
+echo "Input directory: $IN"
+echo "Output directory: $OUT"
+echo "Output surface level file: ${OUTNAME}_surface.sqd"
+echo "Output pressure level file: ${OUTNAME}_pressure.sqd"
+echo "Output hybrid level file: ${OUTNAME}_hybrid.sqd"
+
+
+if [ -z "$DRYRUN" ]; then
+    mkdir -p $OUT/{surface,pressure}/querydata
+    mkdir -p $EDITOR 
+    mkdir -p $TMP
+fi
+
+if [ -n "$DRYRUN" ]; then
+    exit
+fi
+
+function log {
+    echo "$(date -u +%H:%M:%S) $1"
+}
+
+#
+# Surface Data
+#
+if [ ! -s $OUTFILE_SFC ]; then
+    # Convert
+    log "Converting surface grib files to qd files.."
+    gribtoqd -d -t -L 1 \
+	-p "120,UKMO Surface" $PROJECTION \
+	-o $TMP/$(basename $OUTFILE_SFC) \
+	$IN/*${RT_DATE_DDHH}
+
+
+    # Post Process
+    if [ -s $TMP/$(basename $OUTFILE_SFC) ]; then
+	log "Post processing: $(basename $OUTFILE_SFC)"
+        qdscript -a 49 -i $TMP/$(basename $OUTFILE_SFC) $CNF/ukmo-surface.st > $TMP/$(basename $OUTFILE_SFC).tmp
+    fi
+
+    if [ -s $TMP/$(basename $OUTFILE_SFC).tmp ]; then
+	log "Creating Wind and Weather objects: $(basename $OUTFILE_SFC)"
+	qdversionchange -a -g 417 -i $TMP/$(basename $OUTFILE_SFC).tmp 7 > $TMP/$(basename $OUTFILE_SFC)
+    fi
+
+    # Distribute
+    if [ -s $TMP/$(basename $OUTFILE_SFC) ]; then
+	log "Testing: $(basename $OUTFILE_SFC)"
+	if qdstat $TMP/$(basename $OUTFILE_SFC); then
+	    log "Compressing: $(basename $OUTFILE_SFC)"
+	    lbzip2 -k $TMP/$(basename $OUTFILE_SFC)
+	    log "Moving: $(basename $OUTFILE_SFC) to $OUT/surface/querydata/"
+	    mv -f $TMP/$(basename $OUTFILE_SFC) $OUT/surface/querydata/
+	    log "Moving $(basename $OUTFILE_SFC).bz2 to $EDITOR"
+	    mv -f $TMP/$(basename $OUTFILE_SFC).bz2 $EDITOR/
+	else
+	    log "File $TMP/$(basename $OUTFILE_SFC) is not valid qd file."
+	fi
+    fi
+fi # surface
+
+#
+# Pressure Levels
+#
+if [ ! -s $OUTFILE_PL ]; then
+    # Convert
+    log "Converting pressure grib files to $(basename $OUTFILE_PL)"
+    gribtoqd -d -t -L 100  \
+	-p "120,UKMO Pressure" $PROJECTION \
+	-o $TMP/$(basename $OUTFILE_PL) \
+	$IN/*${RT_DATE_DDHH}
+
+    # Post Process
+    if [ -s $TMP/$(basename $OUTFILE_PL) ]; then
+	log "Post processing: $(basename $OUTFILE_PL)"
+	mv -f $TMP/$(basename $OUTFILE_PL) $TMP/$(basename $OUTFILE_PL).tmp
+    fi
+
+    if [ -s $TMP/$(basename $OUTFILE_PL).tmp ]; then
+	log "Creating Wind and Weather objects: $(basename $OUTFILE_PL)"
+	qdversionchange -w 0 -i $TMP/$(basename $OUTFILE_PL).tmp 7 > $TMP/$(basename $OUTFILE_PL)
+    fi
+
+    # Distribute
+    if [ -s $TMP/$(basename $OUTFILE_PL) ]; then
+	log "Testing: $(basename $OUTFILE_PL)"
+	if qdstat $TMP/$(basename $OUTFILE_PL); then
+	    log  "Compressing: $(basename $OUTFILE_PL)"
+	    lbzip2 -k $TMP/$(basename $OUTFILE_PL)
+	    log "Moving: $(basename $OUTFILE_PL) to $OUT/pressure/querydata/"
+	    mv -f $TMP/$(basename $OUTFILE_PL) $OUT/pressure/querydata/
+	    log "Moving: $(basename $OUTFILE_PL).bz2 to $EDITOR/"
+	    mv -f $TMP/$(basename $OUTFILE_PL).bz2 $EDITOR/
+	else
+	    log "File $TMP/$(basename $OUTFILE_PL) is not valid qd file."
+	fi
+    fi
+fi # pressure
+
+log "Cleaning temporary directory $TMP"
+rm -f $TMP/*_ukmo_*
+rmdir $TMP
+
---
+++ smartmet-data-gts-ukmo.spec
@@ -0,0 +1,77 @@
+%define smartmetroot /smartmet
+
+Name:           smartmet-data-gts-ukmo
+Version:        18.12.10
+Release:        1%{?dist}.fmi
+Summary:        SmartMet Data UKMO Global Model from GTS
+Group:          System Environment/Base
+License:        MIT
+URL:            https://github.com/fmidev/smartmet-data-gts-ukmo
+BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+BuildArch:	noarch
+
+Requires:	smartmet-qdtools
+Requires:	lbzip2
+Requires:	vsftpd
+
+
+%description
+TODO
+
+%prep
+
+%build
+
+%pre
+
+%install
+rm -rf $RPM_BUILD_ROOT
+mkdir $RPM_BUILD_ROOT
+cd $RPM_BUILD_ROOT
+
+mkdir -p .%{smartmetroot}/cnf/cron/{cron.d,cron.hourly}
+mkdir -p .%{smartmetroot}/cnf/data
+mkdir -p .%{smartmetroot}/tmp/data
+mkdir -p .%{smartmetroot}/logs/data
+mkdir -p .%{smartmetroot}/run/data/ukmo_gts/{bin,cnf}
+mkdir -p .%{smartmetroot}/data/incoming/gts/ukmo
+
+cat > %{buildroot}%{smartmetroot}/cnf/cron/cron.d/ukmo-gts.cron <<EOF
+# Run every hour to test if new data is available
+5 * * * * /smartmet/run/data/ukmo_gts/bin/doukmo.sh
+EOF
+
+cat > %{buildroot}%{smartmetroot}/cnf/cron/cron.hourly/clean_data_gts_ukmo <<EOF
+#!/bin/sh
+# Clean UKMO data
+cleaner -maxfiles 4 '_ukmo_.*_surface.sqd' /smartmet/data/ukmo
+cleaner -maxfiles 4 '_ukmo_.*_pressure.sqd' /smartmet/data/ukmo
+cleaner -maxfiles 4 '_ukmo_.*_surface.sqd' /smartmet/editor/in
+cleaner -maxfiles 4 '_ukmo_.*_pressure.sqd' /smartmet/editor/in
+
+# Clean incoming SYNOP data older than 1 day (24 * 60 = 1440 min)
+find /smartmet/data/incoming/gts/ukmo/ -type f -mmin +1440 -delete
+EOF
+
+cat > %{buildroot}%{smartmetroot}/cnf/data/ukmo-gts.cnf <<EOF
+AREA="world"
+EOF
+
+install -m 755 %_topdir/SOURCES/smartmet-data-gts-ukmo/doukmo.sh %{buildroot}%{smartmetroot}/run/data/ukmo_gts/bin/
+
+%post
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files
+%defattr(-,smartmet,smartmet,-)
+%config(noreplace) %{smartmetroot}/cnf/data/ukmo-gts.cnf
+%config(noreplace) %{smartmetroot}/cnf/cron/cron.d/ukmo-gts.cron
+%config(noreplace) %attr(0755,smartmet,smartmet) %{smartmetroot}/cnf/cron/cron.hourly/clean_data_gts_ukmo
+%attr(2775,smartmet,gts)  %dir %{smartmetroot}/data/incoming/gts/ukmo
+%{smartmetroot}/*
+
+%changelog
+* Mon Dec 10 2018 Mikko Rauhala <mikko.rauhala@fmi.fi> 18.12.10-1.el7.fmi
+- Initial version
